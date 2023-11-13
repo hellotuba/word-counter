52ea269b144ba00224573f8235c3eca10aca6326
hellotuba <88927076+hellotuba@users.noreply.github.com>
2023-11-13 09:36:17 +0100

    Updated notes in main.c

--- main.c
+++ main.c
@@ -9,15 +9,15 @@ int main() {
     int count = 0;
     int incount = 0;
 
-    //name of the text document
+    //name of the text document (text.txt can be replace with any name)
     file = fopen("text.txt", "r");
 
-    //when the program dont have to open the file
+    //when the program dont have perms to open the file
     if (file == NULL) {
         printf("File cant be open!\n");
         return 1;
     }
-    //counting
+    //counting the words from the file
     while ((c = fgetc(file)) != EOF) {
         if (c == ' ' || c == '\n' || c == '\t') {
             incount = 0;
